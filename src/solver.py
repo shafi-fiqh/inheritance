8392d5e1bbbefa320aeddce0b078e60452101ea1
Omar Khalil <omarkhalil@Omars-MacBook-Pro.local>
2020-09-16 22:22:06 -0400

    added daughter grandaughter

--- src/solver.py
+++ src/solver.py
@@ -19,6 +19,8 @@ def solve(case: dict,
                          descendants=descendants)
     case = solve_wife(case=case,
                       descendants=descendants)
+    case = solve_daughter(case=case)
+    case = solve_granddaughter(case=case)
     #Add more here as we create more partial solvers
     return case
 
@@ -62,7 +64,7 @@ def solve_husband(case: dict,
     return case
 
 def solve_wife(case: dict,
-                 descendants: dict)->dict:
+               descendants: dict)->dict:
     """
     Solve only for the father's share if he exists
     :param case: dictionary of inheritors and shares
@@ -79,4 +81,44 @@ def solve_wife(case: dict,
         case['wife'] = '1/4'
     return case
 
+def solve_daughter(case: dict)->dict:
+    """
+    Solve only for the daughter's share if she exists
+    :param case: dictionary of inheritors and shares
+    :return: string representing the fraction.
+    """
+    if 'daughter' not in case and 'daughter_x2' not in case:
+        return case
+    inh = 'daughter'
+    share = '1/2'
+    if 'daughter_x2' in case:
+        inh = 'daughter_x2'
+        share = '2/3'
+    if 'son' in case:
+        case[inh] = 'A'
+    else:
+        case[inh] = share
+    return case
 
+def solve_granddaughter(case: dict)->dict:
+    """
+    Solve only for the daughter's share if she exists
+    :param case: dictionary of inheritors and shares
+    :return: string representing the fraction.
+    """
+    if 'daughter_of_son' not in case and 'daughter_of_son_x2' not in case:
+        return case
+    inh='daughter_of_son'
+    share='1/2'
+    if 'daughter_of_son_x2' in case:
+        inh = 'daughter_of_son_x2'
+        share = '2/3'
+    if 'son' in case or 'daughter_x2' in case:
+        case[inh] = 0
+    elif 'son_of_son' in case:
+        case[inh] = 'A'
+    elif 'daughter' in case:
+        case[inh] = '1/6'
+    else:
+        case[inh] = share
+    return case
