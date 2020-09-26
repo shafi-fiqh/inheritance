8c757fb41c350698eba5994118f5d97646431041
omkhalil <42871659+omkhalil@users.noreply.github.com>
2020-09-26 12:25:59 -0400

    add solver

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,1 +1,20 @@
+"""
+Testing module
+"""
 import pytest
+import json
+
+from src.cases_generator import CaseGenerator
+from src.solver import solve
+
+def test_cases():
+    with open('config/cases.json', 'r') as cases_file:
+        cases = json.load(cases_file)
+    casegen = CaseGenerator('config/family_config.csv')
+    for case in cases:
+        assert all([case[inh] == solve(case=case,
+                                       descendants=casegen.descendants,
+                                       mahjoob=casegen.mahjoob,
+                                       rank=casegen.rank,
+                                       taseeb=casegen.taseeb)[inh] for inh in case]), 'Case %s failed' % case
+
