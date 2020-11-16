cf3e86f712ced3d1f1510e3d8bb2fa53e8e71680
Saif <saifmahamood@gmail.com>
2020-11-15 21:15:00 -0500

    create mapping and translate from mapping

---
+++ utils/read_arabic_encoding.py
@@ -0,0 +1,18 @@
+from os.path import abspath, dirname, join
+
+import json
+
+
+MAP_FILE = join(dirname(dirname(abspath(__file__))), "config/mapping.json")
+
+
+def read_mapping_file(filepath, encoding='utf-8'):
+    with open(filepath, encoding=encoding) as mapping_file:
+        return json.load(mapping_file)
+
+
+def get_translation(word):
+    mapping_dict = read_mapping_file(MAP_FILE)
+    return mapping_dict.get(word)
+
+print(get_translation("أم"))
