00bc3290a4c6934dc1e9fcaaf466aca0454d250e
Saif <saifmahamood@gmail.com>
2021-03-14 10:35:25 -0400

    add doc string and error handling

--- app/src/generate_unsolved_problems.py
+++ app/src/generate_unsolved_problems.py
@@ -6,7 +6,18 @@ import tqdm
 from utils.helpers import is_redundant
 from utils.helpers import nCr
 
-
+"""
+Generate all possible inheritance scenarios. Consumes a list of all possible
+inheritors, list of inheritors that need to be there, list of inheritors that
+need to be ignored and the total number of inheritors in a given problem
+:param inheritors: List of all possible inheritors
+:param must_haves: List of inheritors that the user wants included in all problems
+:param not_haves: List of inheritors that the user wants ignored in all problems
+:param n_types: number of distinct different types of inheritors.
+For example Daughter + Son is considered 2 types.
+Daugher x 2 + Son is still considered n_types = 2.
+:return: List of JSON objects consisting of inheritance cases to be solved
+"""
 def generate_problems_lst(inheritors:list, must_haves: list, not_haves: list, n_types: int):
     inheritors = [
         inh
