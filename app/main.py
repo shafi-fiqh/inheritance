cfed380a07453d12d66806c31805d94515a725b7
saifmahamood <saifmahamood@gmail.com>
2021-03-14 11:20:54 -0400

    Feature/generate cases endpoint (#44)
    * add endpoint for generating problems
    Co-authored-by: autoblack <omkhalil@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+from flask import abort
 from flask import Flask
 from flask import request
 
 from src.cases_generator import CaseGenerator
 from src.full_solver import full_solver
+from src.generate_unsolved_problems import generate_problems_lst
 from src.solver import solve
 from utils.helpers import calculate_asl
 
@@ -46,6 +48,45 @@ def asl_shares():
         abort(400, "Inheritors must correspond to the config definition")
 
     return calculate_asl(case)
+
+
+@app.route("/generate_problems", methods=["POST"])
+def generate_problems():
+    problem_specs = request.json
+
+    if int(problem_specs["n_types"]) < 1:
+        abort(
+            400,
+            "The number of inheritors in a problem should be greater than or equal to 1",
+        )
+
+    if not (
+        set(problem_specs["must_haves"]) - set(problem_specs["not_haves"])
+        or set(problem_specs["not_haves"]) - set(problem_specs["must_haves"])
+    ):
+        abort(
+            400,
+            "An inheritor cannot be in both the must have and ignore list at the same time",
+        )
+
+    if int(problem_specs["n_types"]) < len(problem_specs["must_haves"]):
+        abort(
+            400,
+            "The total number of inheritos in the case should be greater than the number of inheritors who must be included",
+        )
+
+    if len(set(CASE_GEN.inheritors) - set(problem_specs["not_haves"])) == 0:
+        abort(
+            400,
+            "Cannot generate cases where all inheritors have been asked to be excluded",
+        )
+
+    return generate_problems_lst(
+        inheritors=CASE_GEN.inheritors,
+        must_haves=problem_specs["must_haves"],
+        not_haves=problem_specs["not_haves"],
+        n_types=int(problem_specs["n_types"]),
+    )
 
 
 if __name__ == "__main__":
