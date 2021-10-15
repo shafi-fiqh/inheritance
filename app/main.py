5e974fdd0df231b831a99c588c0a9ee5cf61fc30
Saif <saifmahamood@gmail.com>
2021-10-14 22:41:33 -0400

    one endpoint for evverything

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import copy
+import json
 
 from flask import abort
 from flask import Flask
@@ -47,26 +48,6 @@ def solver():
     return solved_case
 
 
-@app.route("/full_solver", methods=["POST"])
-def complete_solver():
-    case = request.json
-
-    if not all(inh in CASE_GEN.inheritors for inh in case):
-        abort(400, "Inheritors must correspond to the config definition")
-
-    return full_solver(case)
-
-
-@app.route("/asl_shares", methods=["POST"])
-def asl_shares():
-    case = request.json
-
-    if not all(inh in CASE_GEN.inheritors for inh in case):
-        abort(400, "Inheritors must correspond to the config definition")
-
-    return calculate_asl(case)
-
-
 @app.route("/generate_problems", methods=["POST"])
 def generate_problems():
     problem_specs = request.json
@@ -99,15 +80,39 @@ def generate_problems():
             "Cannot generate cases where all inheritors have been asked to be excluded",
         )
 
-    return generate_problems_lst(
-        inheritors=CASE_GEN.inheritors,
-        must_haves=problem_specs["must_haves"],
-        not_haves=problem_specs["not_haves"],
-        n_types=int(problem_specs["n_types"]),
-        grand_father_and_siblings=bool(
-            problem_specs.get("grand_father_and_siblings", "False")
-        ),
-    )
+    cases = generate_problems_lst(
+            inheritors=CASE_GEN.inheritors,
+            must_haves=problem_specs["must_haves"],
+            not_haves=problem_specs["not_haves"],
+            n_types=int(problem_specs["n_types"]),
+            grand_father_and_siblings=bool(
+                problem_specs.get("grand_father_and_siblings", "False")
+            ),
+        )
+
+    ret = []
+    for case in cases:
+        case_obj = {}
+        case_obj['problem'] = copy.deepcopy(case)
+        basic_shares_soln = solve(
+            case=case,
+            descendants=CASE_GEN.descendants,
+            mahjoob=CASE_GEN.mahjoob,
+            rank=CASE_GEN.rank,
+            taseeb=CASE_GEN.taseeb,
+        )
+        case_obj['basic_shares'] = basic_shares_soln
+
+        intermediate_shares_soln = calculate_intermittent_asl(case=case)
+        case_obj['intermediate_shares'] = intermediate_shares_soln
+
+        if need_final_solver(intermediate_shares_soln):
+            case_obj["final_shares"] = calculate_asl(
+                full_solver(copy.deepcopy(basic_shares_soln))
+            )
+        ret.append(case_obj)
+
+    return ret
 
 
 if __name__ == "__main__":
