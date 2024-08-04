9da10601cd2abb9bfc12d44aa3a93bce822273c0
autoblack <omkhalil@users.noreply.github.com>
2024-08-04 18:13:17 +0000

    fixup: Format Python code with Black

--- app/utils/sort_heirs.py
+++ app/utils/sort_heirs.py
@@ -1,10 +1,14 @@
 def order_dict_by_keys(inh_dict: dict) -> dict:
     # Determine the first key ('husband' or 'wife')
-    first_key = next((key for key in ['husband', 'wife'] if key in inh_dict), None)
-    
+    first_key = next((key for key in ["husband", "wife"] if key in inh_dict), None)
+
     # Separate the rest into fixed shares inheritors and universal heir inheritors
-    fixed_inh = {key: value for key, value in inh_dict.items() if key != first_key and value != 'U'}
-    universal_inh = {key: value for key, value in inh_dict.items() if value == 'U'}
+    fixed_inh = {
+        key: value
+        for key, value in inh_dict.items()
+        if key != first_key and value != "U"
+    }
+    universal_inh = {key: value for key, value in inh_dict.items() if value == "U"}
 
     # Construct the ordered dictionary
     ordered_dict = {}
@@ -15,7 +19,10 @@ def order_dict_by_keys(inh_dict: dict) -> dict:
 
     return ordered_dict
 
+
 def order_inhs_according_to_sorted(sorted_inh: dict, unsorted_inh: dict) -> dict:
     # Create a new dictionary with keys from sorted_dict and values from unsorted_dict
-    ordered_dict = {key: unsorted_inh.get(key) for key in sorted_inh if key in sorted_inh}
-    return ordered_dict 
+    ordered_dict = {
+        key: unsorted_inh.get(key) for key in sorted_inh if key in sorted_inh
+    }
+    return ordered_dict
